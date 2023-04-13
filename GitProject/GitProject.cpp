ae33f35220580fb3534860561510a69b96b42985
Пользователь <vexese123@gmail.com>
2023-04-13 17:50:24 +0300

    First commit

---
+++ GitProject/GitProject.cpp
@@ -0,0 +1,19 @@
+﻿// DZ 7 №4
+
+#include <iostream>
+#include <string>
+int main()
+{
+	std::string flname;
+	std::string fname, lname;
+	std::cout << "Enter your name: ";
+	std::getline(std::cin, flname);
+	std::string temp = flname;
+	int pos = temp.find(" ");
+	fname = temp.erase(pos);
+	lname = flname.erase(0, pos);
+
+	std::cout << "first name is " << fname << std::endl;
+	std::cout << "last name is " << lname << std::endl;
+
+}
